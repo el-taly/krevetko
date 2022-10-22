624b3be964b5e2141603dfb97ee6c871e3f1204c
AnnaV <anna@a-ventura.ru>
2022-10-22 22:47:18 +0300

    EXAMPLES_006

---
+++ EXAMPLS_006/Program.cs
@@ -0,0 +1,12 @@
+﻿int A=1;
+int B=8;
+int C=3;
+int D=2;
+int E=6;
+int max=A;
+if(B>max) max=B;
+if(C>max) max=C;
+if(D>max) max=D;
+if(E>max) max=E;
+Console.Write("max=");
+Console.WriteLine(max);
