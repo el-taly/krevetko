36ef253aad267282f6e8572b604324bac8d92bc7
AnnaV <anna@a-ventura.ru>
2022-10-23 18:08:53 +0300

    sravnenie 3eh chisel

---
+++ DZ2/Program.cs
@@ -0,0 +1,21 @@
+﻿int a=22, b=3, c=9;
+if (a>b)
+{
+if (a>c)
+{
+Console.WriteLine(a);
+}
+else
+{
+Console.WriteLine(c);
+}
+}
+else
+if (b>c)
+{
+Console.WriteLine(b);
+}
+else
+{
+Console.WriteLine(c);
+}
