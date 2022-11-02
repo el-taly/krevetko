3591b706dcfa0f64ec02ce910e6a9baea4758702
AnnaV <anna@a-ventura.ru>
2022-11-02 18:06:19 +0300

    DZ5

---
+++ DZ5/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число дня недели от 1 до 7: ");
+int numb=int.Parse(Console.ReadLine()!);
+Console.WriteLine("Вы ввели число дня недели, " +numb);
+
+if(numb == 6 || numb==7)
+{
+ Console.WriteLine("Выходной"); 
+}
+else
+{
+  Console.WriteLine("Будний день"); 
+}
