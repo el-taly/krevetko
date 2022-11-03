fb3471477a86edd5c82966db0240fc5c10895f24
AnnaV <anna@a-ventura.ru>
2022-11-03 22:11:46 +0300

    DZ6

---
+++ DZ6/Program.cs
@@ -0,0 +1,26 @@
+﻿// По 2-ум аданным числам проверить, является ли одно квадратом другого
+Console.Clear();
+Console.WriteLine("Введите 1-ое число: ");
+int numb=int.Parse(Console.ReadLine()!);
+Console.WriteLine("Вы ввели число: " +numb);
+Console.WriteLine("Введите 2-ое число: ");
+int var=int.Parse(Console.ReadLine()!);
+Console.WriteLine("Вы ввели число: " +var);
+
+if(numb * numb ==var)
+{
+ Console.Write(var);
+ Console.WriteLine (" квадрат " +numb); 
+}
+else
+
+    if(var * var ==numb)
+{
+ Console.Write(numb);
+ Console.WriteLine (" квадрат " +var); 
+}
+
+else
+{
+  Console.WriteLine("Фигня"); 
+}
