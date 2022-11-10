9c3f3c8f955c7a81241d80647df029fef2aa192f
AnnaV <anna@a-ventura.ru>
2022-11-10 22:19:48 +0300

    DZ7

---
+++ DZ7/Program.cs
@@ -0,0 +1,25 @@
+﻿//задать номер четверти, показать диапазон возможных
+//координат
+
+Console.Clear();
+Console.WriteLine("Введите первую координату четверти: ");
+int x=int.Parse(Console.ReadLine()?? "0");
+Console.WriteLine("Введите вторую координату четверти: ");
+int Y=int.Parse(Console.ReadLine()?? "0");
+if(x>0 && Y>0)
+{
+Console.WriteLine("Первая четверть: ");
+}
+
+if(x<0 && Y>0)
+{
+Console.WriteLine("Вторая четверть: ");
+}
+if(x<0 && Y<0)
+{
+Console.WriteLine("Третья четверть: ");
+}
+if(x>0 && Y<0)
+{
+Console.WriteLine("Чтвертая четверть: ");
+}
