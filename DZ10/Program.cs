332b066e43a3c0a2832253e271fe2ec09e3c803f
AnnaV <anna@a-ventura.ru>
2022-11-11 20:35:42 +0300

    DZ10

---
+++ DZ10/Program.cs
@@ -0,0 +1,8 @@
+﻿//Программа, которая принимает на вход 3-ех значное число и показывает вторую цифру
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int numb=int.Parse(Console.ReadLine()?? "0");
+int key=numb/10;
+Console.WriteLine("Частное от деления трехзначного числа на 10 =: " +key);
+int ost=key%10;
+Console.WriteLine("Вторая цифра трехзначного числа =: " +ost);
