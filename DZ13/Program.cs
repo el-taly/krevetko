f0baefb4d275ab3aa4de23915ca3587aa105612f
AnnaV <anna@a-ventura.ru>
2022-12-07 15:41:44 +0300

    DZ13

---
+++ DZ13/Program.cs
@@ -0,0 +1,21 @@
+﻿//Программа, которая принимает на вход число и показывает 3-ю цифру или
+//сообщает, что ее нет
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int numb=int.Parse(Console.ReadLine()?? "0");
+if (numb>1 && numb<100)
+Console.WriteLine("У числа нет третьей цифры");
+else
+{
+    while (numb>1000)
+    {
+     numb=numb/10;
+     
+ 
+    }
+    int ost=numb%10;
+
+Console.WriteLine("Третья цифра числа =: " +ost);
+}
+
