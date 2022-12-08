4967fd6259d2c5ef4f91e57ed52e98b70f2b4e51
AnnaV <anna@a-ventura.ru>
2022-12-09 01:33:09 +0300

    DZ36

---
+++ DZ36/Program.cs
@@ -0,0 +1,29 @@
+﻿// массив, наполненный случайными числами
+// написать программу, которая покажет сумму нечетных элементов
+
+using System;
+using static System.Console;
+
+Clear();
+
+
+int[] GetArrey(int size, int minValue, int maxValue)
+{
+    int[] res =new int[size];
+    for (int i=0; i<size; i++)
+    {
+        res[i]= new Random().Next(minValue, maxValue+1);
+        Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+int[] array=GetArrey(10,-100,100);
+int summ=0;
+
+foreach (int mau in array)
+{
+    if (mau%2!=0) summ=summ+mau;
+}
+
+WriteLine();
+WriteLine(summ);
