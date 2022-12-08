e5e85b130c347869e7183f6d0420f9b10c4448b6
AnnaV <anna@a-ventura.ru>
2022-12-08 13:07:44 +0300

    DZ25

---
+++ DZ25/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите A: ");
+int A=int.Parse(ReadLine());
+
+Write("Введите B: ");
+int B=int.Parse(ReadLine());
+
+
+WriteLine($"{Math.Pow(A,B)}");
+
