37da165911db1bfdb58c476d1eaabedaab7a2b10
AnnaV <anna@a-ventura.ru>
2022-12-08 14:30:01 +0300

    DZ29

---
+++ DZ29/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int [] arr={1, 2, 5, 7, 19, 6, 1, 33};
+
+void PrintArray (int [] array)
+{
+    int count=array.Length;
+    for(int i=0; i<count; i++)
+    {
+        Write($"{array[i]} ");
+    }
+    WriteLine();
+}
+PrintArray(arr);
