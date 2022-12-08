6558954dd6b09c1304111bd886ca3d0db4fa419b
AnnaV <anna@a-ventura.ru>
2022-12-08 16:37:29 +0300

    DZ27

---
+++ DZ27/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите число: ");
+int numb=int.Parse(ReadLine());
+
+int summ=0;
+while(numb>0)
+{
+   summ=summ+numb%10;
+   numb=numb/10;
+}
+
+WriteLine($"Сумма цифр в числе = {summ}");
+
+
