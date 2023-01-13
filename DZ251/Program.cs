770c3972d3d9fc4f8090455107e3437181206b2a
AnnaV <anna@a-ventura.ru>
2023-01-13 16:20:56 +0300

    DZ251

---
+++ DZ251/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать цикл, который на вход принимает 2 числа А и В и возводит число А в степень В
+using System;
+using static System.Console;
+
+Clear();
+int v = 1;
+
+Write("Введите A: ");
+int A=int.Parse(ReadLine());
+
+Write("Введите B: ");
+int B=int.Parse(ReadLine());
+
+for(int i=1; i<=B; i++)
+    {
+        v=v*A;
+    
+    }
+WriteLine(v);
+
