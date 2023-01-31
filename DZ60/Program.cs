6603b9754619c751cb65447b474c5e4aaf194398
AnnaV <anna@a-ventura.ru>
2023-02-01 00:37:23 +0300

    Сформировать трехмерный массив из неповторяющихся чисел

---
+++ DZ60/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задать трехмерный массив, из неповторяющихся двухзначных чисел
+// написать программу, которая будет выводить построчно массив, добавляя индексы каждого элемента
+
+using System;
+using static System.Console;
+
+Clear();
+
+void FillArray(int [ , , ] matr)
+{
+    
+    
+for (int i=0; i<matr.GetLength(0); i++)
+{
+    WriteLine("1 измерение: " + i);
+    for (int j=0; j<matr.GetLength(1); j++)
+    {
+        for (int k=0; k<matr.GetLength(2); k++)
+    {
+        matr[i,j,k]=new Random().Next(10,99);
+        //Write($"{matr[i ,j , k]} ({i},{j},{k}), | ");
+        for (int z = 0; z < k; z++)
+        {
+              if (matr[i, j, z] == matr[i, j, k])
+                 k--;
+        }
+       Write($"{matr[i ,j , k]} ({i},{j},{k}), | ");
+    }
+     
+   WriteLine();
+}
+WriteLine("=============================");
+}
+}
+
+
+int[,,] matrix = new int[2,2,2];
+
+
+Clear();
+FillArray(matrix);
+WriteLine();
