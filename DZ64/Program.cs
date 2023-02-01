f492c02083d9f74f1f052a75abf8632cced00889
AnnaV <anna@a-ventura.ru>
2023-02-02 01:01:57 +0300

    Рекурсия, массив натуральных чисел от N до 1

---
+++ DZ64/Program.cs
@@ -0,0 +1,28 @@
+﻿// программа принимает на вход число N
+// помощью рекурсии выводит все натуральные числа от N до 1
+
+
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите число: ");
+int N=int.Parse(ReadLine());
+WriteLine();
+
+
+
+string NumberRec(int a, int N)
+{
+    if (a<=N) return NumberRec(a+1,N) + $"{a} ";
+    else return String.Empty;
+}
+
+WriteLine(NumberRec(1,N));
+
+
+
+
+
