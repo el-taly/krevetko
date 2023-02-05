8dc03c041b89e7f84908b4cd3f3fea3b84d684f1
AnnaV <anna@a-ventura.ru>
2023-02-05 17:35:31 +0300

    задача на рекурсию 2

---
+++ DZ66/Program.cs
@@ -0,0 +1,27 @@
+﻿// программа принимает на вход числа M и N
+// помощью рекурсии находит сумму всех натуральных элементов между ними
+
+
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите число: ");
+int N=int.Parse(ReadLine());
+WriteLine();
+
+Write("Введите число: ");
+int M=int.Parse(ReadLine());
+WriteLine();
+
+
+
+string NumberRec(int M, int N)
+{
+    if (M<=N) return NumberRec(M+1,N) + $"{M} ";
+    else return String.Empty;
+}
+
+WriteLine(NumberRec(M,N));
