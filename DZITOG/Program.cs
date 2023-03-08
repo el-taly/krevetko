2dfd138baa071c393dd8813357e44b1b64e47a07
AnnaV <anna@a-ventura.ru>
2023-03-08 12:52:09 +0300

    Из массива строк сделать массив, равный 3-ем символам

---
+++ DZITOG/Program.cs
@@ -0,0 +1,10 @@
+﻿// из имеющегося массива строк
+// сделать массив меньше либо равным 3-ем символам
+
+
+Console.Clear();
+
+string[] strings = { "hello", "2", ":-"}; 
+var result = strings.Where(s => s.Length <= 3).ToArray(); 
+Console.WriteLine(string.Join(", ", result)); 
+Console.ReadLine();
